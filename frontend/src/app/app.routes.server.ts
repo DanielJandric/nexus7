b2431228fa259231c73b348e05db45da7b89ecfe
DanielJandric <daniel.jandric@becapital.ch>
2025-06-06 07:28:51 +0200

    fix: remove SSR and fix Angular production build

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,8 +1,1 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
-
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
-];
+// This file is intentionally left blank as SSR is not being used.
